de88db4cbc09db8450c981312a0b5da0abac6d7b
Deepender <deepender07@gmail.com>
2017-09-14 17:26:02 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+// Component Imports
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+// Service Imports
+import { ClientService } from './services/client.service';
 
+const appRoutes: Routes = [
+  {path:'', component:DashboardComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'login', component:LoginComponent}
+];
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCcM-mK4GvkVrn_j0YC1RZXXPnGRMrQcyU",
+  authDomain: "ang4client-a6a32.firebaseapp.com",
+  databaseURL: "https://ang4client-a6a32.firebaseio.com",
+  storageBucket: "ang4client-a6a32.appspot.com",
+  messagingSenderId: "458253138409"
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {  }
